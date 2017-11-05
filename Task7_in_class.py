316efd41b9da14983c08940b025321ca5e85d8d3
BFilinov <borfiliev@mail.ru>
2017-11-05 15:05:10 +0300

    Домашка по 7 занятию - декораторы

--- Task7_in_class.py
+++ Task7_in_class.py
@@ -1,3 +1,7 @@
+import time
+
+
+# 1
 def is_palindrome(s):
     from functools import reduce
     s_trim = list(filter(lambda char: char != ' ', s))
@@ -5,5 +9,48 @@ def is_palindrome(s):
     return reduce(lambda x, y: x + y, checksum) == 0
 
 
-print(is_palindrome('а роза упала на лапу азора'))
-print(is_palindrome('not a palindrome'))
+# 2
+def func_working_time(func):
+    def func_inner(string):
+        stamp1 = time.time()
+        ret = func(string)
+        stamp2 = time.time()
+        exec_time = stamp2 - stamp1
+        print('Execution complete: {}'.format(exec_time))
+        print('Value={}'.format(ret))
+        return ret
+
+    return func_inner
+
+
+# 3
+def switch(enabled=True):
+    def func_inner_1(func):
+        def func_inner_2(string):
+            if not enabled:
+                return func(string)
+            stamp1 = time.time()
+            ret = func(string)
+            stamp2 = time.time()
+            exec_time = stamp2 - stamp1
+            print('Execution complete: {}'.format(exec_time))
+            print('Value={}'.format(ret))
+            return ret
+
+        return func_inner_2
+
+    return func_inner_1
+
+
+@switch(enabled=True)
+def test_function(string):
+    if len(string) > 5:
+        print('Hey I am banana')
+    print("And that's all folks")
+
+
+# decorated = func_working_time(is_palindrome)
+# decorated('а роза упала на лапу азора')
+# decorated('not a palindrome')
+test_function('YoloSwagg')
+test_function('Nope')
